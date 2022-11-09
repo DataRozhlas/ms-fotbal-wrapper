c47981a14638cbe02b406b9aceb15774cc4db58d
Petr Kočí <petr.koci@samizdat.cz>
2022-11-09 18:10:55 +0100

    layout, branding, iframe

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 ﻿import "./byeie"; // loučíme se s IE
 
+window.addEventListener("message", function (a) {
+  if (void 0 !== a.data["cro-embed-height"])
+    for (var e in a.data["cro-embed-height"])
+      if ("cro_fotbal_tipovacka" == e) {
+        var d = document.querySelector("#cro_fotbal_tipovacka");
+        d && (d.style.height = a.data["cro-embed-height"][e] + "px");
+      }
+});
+
 /*
 // snadné načtení souboru pro každého!
 fetch("https://blabla.cz/blabla.json")
